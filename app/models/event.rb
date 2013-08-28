569b870cf61ab0b422233cef0dcf2af3e505a209
Justin Leveck <jleveck@gmail.com>
2013-08-27 23:19:22 -0700

    Generate Event model
    Tests pass

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,3 @@
 class Event < ActiveRecord::Base
+  validates :user_id, :start_date, :end_date, :start_time, :end_time, :location_id, presence: true
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,41 @@
 require 'spec_helper'
 
 describe Event do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:event) do
+    Event.new(user_id: 1, start_date: Date.today, end_date: Date.today, start_time: Time.now, end_time: Time.now + 1, location_id: 1)
+  end
+
+  it 'is valid' do
+    expect(event).to be_valid
+  end
+
+  it 'is invalid without start date' do
+    event.start_date = nil
+    expect(event).not_to be_valid
+  end
+
+  it 'is invalid without end date' do
+    event.end_date = nil
+    expect(event).not_to be_valid
+  end
+
+  it 'is invalid without a user id' do
+    event.user_id = nil
+    expect(event).not_to be_valid
+  end
+
+  it 'is invalid without a start time' do
+    event.start_time = nil
+    expect(event).not_to be_valid
+  end
+
+  it 'is invalid without a end time' do
+    event.end_time = nil
+    expect(event).not_to be_valid
+  end
+
+  it 'is invalid without a location' do
+    event.location_id = nil
+    expect(event).not_to be_valid
+  end
 end
